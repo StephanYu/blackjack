086c4711d39e672f5c80386c7d3fc8af0e378874
Stephan Yu <Stephan.yu@gmail.com>
2014-11-19 11:11:56 +0100

    added game and new player views

--- main.rb
+++ main.rb
@@ -3,6 +3,31 @@ require 'sinatra'
 
 set :sessions, true
 
+get '/' do
+  if session[:player_name]
+    redirect '/game'
+  else
+    redirect '/new_player'
+  end
+end
+
+get '/new_player' do
+  erb :new_player
+end
+
+post '/new_player' do
+  session[:player_name] = params[:player_name]
+  redirect '/game'
+end
+
+get '/game' do
+  suits = ['H', 'D', 'C', 'S']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle!
+  erb :game
+end
+
+
 
 
 
